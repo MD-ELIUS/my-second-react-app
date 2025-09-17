843b737201693e1255b4f3e90adfb3f5e23cc165
MD-ELIUS <faysal@mislbd.com>
2025-09-17 20:09:21 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Batsman from './Batsman'
 import Counter from './counter'
+import Users from './Users'
 function App() {
 
   function handleClick() {
@@ -22,6 +23,8 @@ function App() {
     <>
      
       <h3>Vite + React</h3>
+
+      <Users></Users>
       <Batsman></Batsman>
 
       <Counter></Counter>
--- src/Summary.jsx
+++ src/Summary.jsx
@@ -6,3 +6,18 @@
  * Events
  * [ Conditional Rendering]
  */
+
+/**
+ * 1.API : url: https://jsonplaceholder.typicode.com/users
+ */
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(res => res.json())
+.then(data => console.log(data))
+
+const loadData = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = res.json();
+    return data
+}
